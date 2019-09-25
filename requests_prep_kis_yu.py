4c169ac94ca5955758c6bcf192e18e19808f34b5
wrackspurt <51002852+wrackspurt@users.noreply.github.com>
2019-09-25 15:59:35 +0300

    Update requests_prep_kis_yu.py

--- requests_prep_kis_yu.py
+++ requests_prep_kis_yu.py
@@ -1,11 +1,11 @@
-from bs4 import BeautifulSoup
 import json
-from datetime import datetime, date
+import requests
+from bs4 import BeautifulSoup
+from datetime import date
 
-
-BASE_URL = 'https://habr.com/ru/news/'
-
-creation_date = date(year=2019, month=9, day=24)
+base_url = 'https://habr.com/ru/news/'
+creation_date = date(year=2019, month=9, day=25)
+jpath = 'articles.json'
 
 
 def get_html_page(url):
@@ -24,22 +24,29 @@ def find_articles(html_page):
     return title_list
 
 
-#def publish_report(title_list, path):
-    dictionary = {
-        'url': BASE_URL,
-        'date': '2019-09-24',
-        'articles': title_list
-    } 
- #  filename='aricles.json'
-   #with open(filename, 'w') as fp:
-    # print(json.dump(dictionary, fp)) 
-   # rh = dict(title_list)
-   # print(json.dumps(rh))
+def publish_report(path, articles):
+    articles_dict = []
+    for i in range(len(articles)):
+        articles_dict.append({"title": articles[i]})
+    titles = {
+        "url": base_url,
+        "creationDate": str(creation_date),
+        "articles": articles_dict
+    }
+    try:
+        data = json.load(open(path))
+    except:
+        data = []
+
+    data.append(titles)
+
+    with open(path, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
 
 
 def main():
 
-    print(find_articles(get_html_page(BASE_URL)))
+    publish_report(jpath, find_articles(get_html_page(base_url)))
 
 
 if __name__ == '__main__':
