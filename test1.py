b2ac219816aee1d47ecbfb1d6caa14d325f249f7
yureva00 <yureva.00@mail.ru>
2019-10-01 15:35:14 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,33 +2,32 @@ import unittest
 import json
 
 
-def check_out(afile):
+def check_out(filename, encoding='utf-8'):
     check = 0
-    with open(afile, "r") as file:
-        data_s = json.load(file) # переделать: написать обычное чтение из файла без load
+    with open(filename,  encoding='utf-8') as file:
+        data_s = json.load(file)
     if data_s["url"] == "https://habr.com/ru/news/":
         check = 1
     if len(data_s["articles"]) >= 1:
         check = 1
     for i in data_s["articles"]:
         for k in i:
-            if i[k] != None:
+            if i[k]!= None:
                 check = 1
                 break
-    return(check)
+    return check
 
-"""
+
 class TestPage(unittest.TestCase):
     def test_html_page(self):
         result = check_out('articles.json')
         self.assertEqual(result, 1)
-        
-        if __name__ == '__main__':
+
+
+if __name__ == '__main__':
     unittest.main()
-"""
 
 
-print(check_out('articles.json'))
 
 
 
