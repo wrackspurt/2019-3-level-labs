9119e630b4af8b8df78a07b65bf0f774f6c89dd8
yureva00 <yureva.00@mail.ru>
2019-10-01 15:45:33 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,12 +1,13 @@
 import unittest
 import json
+from requests_prep_kis_yu import base_url
 
 
-def check_out(filename, encoding='utf-8'):
+def check_out(filename):
     check = 0
     with open(filename,  encoding='utf-8') as file:
         data_s = json.load(file)
-    if data_s["url"] == "https://habr.com/ru/news/":
+    if data_s["url"] == base_url:
         check = 1
     if len(data_s["articles"]) >= 1:
         check = 1
