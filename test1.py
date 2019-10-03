2fc83a1fa14d13df2ce59121a685dc25cc1faba4
wrackspurt <51002852+wrackspurt@users.noreply.github.com>
2019-10-03 19:39:33 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,23 +2,6 @@ import unittest
 import json
 from requests_prep_kis_yu import base_url
 
-"""
-def check_out(filename):
-    check = 0
-    with open(filename,  encoding='utf-8') as file:
-        data_s = json.load(file)
-    if data_s["url"] == base_url:
-        check = 1
-    if len(data_s["articles"]) >= 1:
-        check = 1
-    for i in data_s["articles"]:
-        for k in i:
-            if i[k]!= None:
-                check = 1
-                break
-    return check
-"""
-
 
 def check_out(filename):
     check1 = 0
