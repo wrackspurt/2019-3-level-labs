ed1e7c1000fc022d346e0c8615c49e5caa4bd65a
yureva00 <yureva.00@mail.ru>
2019-10-03 12:14:26 +0300

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,11 @@
+import unittest
+from requests_prep_kis_yu import find_articles
+
+
+def check_out1():
+    flag = 0
+    length_of_articles = len(find_articles.title_list)
+    print(length_of_articles)
+
+
+print(check_out1)
