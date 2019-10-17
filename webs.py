c24524d4aadbda787485cd9ec878963d92cdda7a
yureva00 <yureva.00@mail.ru>
2019-10-17 17:28:29 +0300

    Update webs.py

--- webs.py
+++ webs.py
@@ -14,10 +14,20 @@ def habr_articles():
         return render_template('index.html', habr_articles=dtitles['articles'], habr_url=dtitles)
 
 
+@app.route('/html_page', methods=['GET'])
+def html_page():
+    return render_template('index1.html')
+
+
 @app.route('/update_page', methods=['POST'])
 def update_page():
     return redirect(url_for('habr_articles'))
 
 
+@app.route('/update2_page', methods=['POST'])
+def update2_page():
+    return redirect(url_for('html_page'))
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=7000, debug=True)
