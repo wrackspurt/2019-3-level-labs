3f0578eb9260b7f5dd962b7b0939985f6152b297
wrackspurt <51002852+wrackspurt@users.noreply.github.com>
2019-10-24 09:52:41 +0300

    Update requests_prep_kis_yu.py

--- requests_prep_kis_yu.py
+++ requests_prep_kis_yu.py
@@ -5,6 +5,7 @@ import datetime
 
 
 base_url = 'https://habr.com/ru/news/'
+another_url = 'https://arstechnica.com/gadgets/'
 current = datetime.datetime.now()
 creation_date = current.strftime("%d-%m-%y")
 jpath = 'articles.json'
@@ -26,17 +27,35 @@ def find_articles(html_page):
     return title_list
 
 
-def publish_report(path, articles):
+def find_another_articles(html_page):
+    parsed_page = BeautifulSoup(html_page, 'html.parser')
+    headings = parsed_page.find_all('h2')
+    quantity = len(headings)
+    title_list = list()
+    for i in range(quantity):
+        title_list.append(headings[i].text)
+    return title_list
+
+
+def publish_report(path, articles, another_articles):
     articles_dict = []
+    another_articles_dict = []
     for i in range(len(articles)):
         articles_dict.append({"title": articles[i]})
+    for j in range(len(another_articles)):
+        another_articles_dict.append({"title": another_articles[j]})
     titles = {
-        "url": base_url,
         "creationDate": creation_date,
-        "articles": articles_dict
+        "habrUrl": base_url,
+        "habrArticles": articles_dict,
+        "arsUrl": another_url,
+        "arsArticles": another_articles_dict
     }
 
     with open(path, 'w', encoding='utf-8') as file:
         json.dump(titles, file, indent=2, ensure_ascii=False)
 
+
+if __name__ == '__main__':
+    publish_report(jpath, find_articles(get_html_page(base_url)), find_another_articles(get_html_page(another_url)))
 
